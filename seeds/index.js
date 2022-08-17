1c3f3e6bb292779bbe6b8059e808d176bc4d90bd
Kim Jaejung <jaejung0412@gmail.com>
2022-08-18 01:23:36 +0900

    클러스터맵 기능 탑재
    클러스터맵 기능 탑재

--- seeds/index.js
+++ seeds/index.js
@@ -27,6 +27,13 @@ const seedDB = async () => {
       location: `${cities[random1000].city}, ${cities[random1000].state}`,
       title: `${sample(descriptors)} ${sample(places)}`,
       image: "https://source.unsplash.com/collection/483251",
+      geometry: {
+        type: "Point",
+        coordinates: [
+          cities[random1000].longitude,
+          cities[random1000].latitude,
+        ],
+      },
       description:
         "Lorem ipsum dolor sit amet consectetur adipisicing elit. Quibusdam dolores vero perferendis laudantium, consequuntur voluptatibus nulla architecto, sit soluta esse iure sed labore ipsam a cum nihil atque molestiae deserunt!",
       price,
