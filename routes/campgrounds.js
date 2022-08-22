3656bc161bbbb9dac71dd927b30b768e9f16c446
Kim Jaejung <jaejung0412@gmail.com>
2022-08-23 00:12:29 +0900

    multer 설치 밒 인코딩 확인
    multer 설치 및 인코딩 확인

--- routes/campgrounds.js
+++ routes/campgrounds.js
@@ -3,15 +3,21 @@ const router = express.Router({ mergeParams: true });
 const campgrounds = require("../controllers/campgrounds");
 const catchAsync = require("../utils/catchAsync");
 const { isLoggedIn, isAuthor, validateCampground } = require("../middleware");
+const multer = require("multer");
+const upload = multer({ dest: "uploads/" });
 
 router
   .route("/")
   .get(catchAsync(campgrounds.index))
-  .post(
-    isLoggedIn,
-    validateCampground,
-    catchAsync(campgrounds.createCampground)
-  );
+  // .post(
+  //   isLoggedIn,
+  //   validateCampground,
+  //   catchAsync(campgrounds.createCampground)
+  // );
+  .post(upload.array("image"), (req, res) => {
+    console.log(req.body, req.files);
+    res.send("동작하나?");
+  });
 
 router.get("/new", isLoggedIn, campgrounds.renderNewForm);
 
